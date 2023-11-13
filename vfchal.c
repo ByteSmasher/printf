ba8886158540d5a41e2dcef1161e3961d959544d
Princewill Olaiya <princewillolaiya@gmail.com>
2023-11-13 18:29:33 +0100

    precision fix

--- vfchal.c
+++ vfchal.c
@@ -10,33 +10,28 @@
  */
 
 
-char *get_precision(char *p, params_t *params, va_list ap) {
+char *get_precision(char *p, params_t *params, va_list ap)
 
-    int d = 0;
+{
 
-    // Check if the current character is a dot (.)
-    if (*p != '.') {
-        return p; // If not, return the current pointer
-    }
+	int d = 0;
 
-    p++; // Advance to the next character
+	if (*p != '.')
+		return (p);
+	p++;
+	if (*p == '*')
+	{
+		d = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
 
-    // Handle optional '*' modifier for dynamic precision
-    if (*p == '*') {
-        // Extract the dynamic precision value from the argument list
-        d = va_arg(ap, int);
-        p++; // Advance to the next character
-    } else {
-        // Parse the precision value from the format string
-        while (_isdigit(*p)) {
-            d = d * 10 + (*p++ - '0');
-        }
-    }
+		while (_isdigit(*p))
+			d = d * 10 + (*p++ - '0');
 
-    // Store the extracted precision value in the params structure
-    params->precision = d;
+	}
+	params->precision = d;
+	return (p);
 
-    // Return the pointer to the next character after the precision specifier
-    return p;
 }
-
